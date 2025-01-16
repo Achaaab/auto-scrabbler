64485f58d42a85b11e457ef1c755a7e38adb48d9
Jonathan Guéhenneux <jonathan.guehenneux@gmx.fr>
2025-01-16 23:49:33 +0100

    add some unit tests

--- src/test/java/com/github/achaaab/scrabble/model/TileCollectionTest.java
+++ src/test/java/com/github/achaaab/scrabble/model/TileCollectionTest.java
@@ -2,7 +2,11 @@ package com.github.achaaab.scrabble.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static com.github.achaaab.scrabble.model.Tile.BLANK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit tests of {@link TileCollection}.
@@ -21,6 +25,66 @@ class TileCollectionTest {
 		tileCollection.add(tile);
 
 		assertEquals(1, tileCollection.size());
-		assertEquals(tile, tileCollection.pick('A'));
+	}
+
+	@Test
+	void clear() {
+
+		var tileCollection = new Bag();
+
+		tileCollection.clear();
+		assertTrue(tileCollection.isEmpty());
+
+		var tile = new Tile('A', 1);
+		tileCollection.add(tile);
+		tileCollection.clear();
+		assertTrue(tileCollection.isEmpty());
+	}
+
+	@Test
+	void addAll() {
+
+		var tileCollection = new Bag();
+
+		var tile0 = new Tile('A', 1);
+		var tile1 = new Tile('S', 1);
+		tileCollection.addAll(List.of(tile0, tile1));
+
+		assertEquals(2, tileCollection.size());
+	}
+
+	@Test
+	void removeAll() {
+
+		var tileCollection = new Bag();
+
+		var tile0 = new Tile('A', 1);
+		var tile1 = new Tile('B', 3);
+		var tile2 = new Tile('C', 3);
+
+		tileCollection.addAll(List.of(tile0, tile1, tile2));
+		tileCollection.removeAll(List.of(tile0, tile1));
+
+		assertEquals(1, tileCollection.size());
+	}
+
+	@Test
+	void remove() {
+
+		var tileCollection = new Bag();
+
+		var tile0 = new Tile('A', 1);
+		var tile1 = new Tile('B', 3);
+		var tile2 = new Tile(BLANK, 0);
+		tileCollection.addAll(List.of(tile0, tile2));
+
+		tileCollection.remove(tile0);
+		assertEquals(1, tileCollection.size());
+
+		tileCollection.remove(tile1);
+		assertEquals(1, tileCollection.size());
+
+		tileCollection.remove(tile2);
+		assertTrue(tileCollection.isEmpty());
 	}
 }
